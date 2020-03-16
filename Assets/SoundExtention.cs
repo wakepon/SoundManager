2f4b2eed3a78f88e67a5e48c6ea2b317e7cfe990
waken <wakepon@gmail.com>
2020-03-16 11:49:59 +0900

    this.PlaySe( name, delay ) returns SoundManager.Handle.

--- Assets/SoundExtention.cs
+++ Assets/SoundExtention.cs
@@ -8,11 +8,12 @@ public static class SoundExtentions
         return SoundManager.Instance.PlaySe(name);
     }
 
-    public static void PlaySe(this MonoBehaviour behaviour, string name, float delay)
+    public static SoundManager.Handle PlaySe(this MonoBehaviour behaviour, string name, float delay)
     {
-        behaviour.StartCoroutine(PlaySeProcess(behaviour, name, delay));
+        return SoundManager.Instance.PlaySe(name,delay);
     }
 
+    //Use this if you might want to stop while waiting for PlaySe.
     public static IEnumerator PlaySeProcess(this MonoBehaviour behaviour, string name, float delay)
     {
         yield return new WaitForSeconds(delay);
